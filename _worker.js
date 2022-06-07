bdf6c633d72275d5c101a096723a6907a50e207f
gvh76hbnjkmi <104751143+gvh76hbnjkmi@users.noreply.github.com>
2022-06-07 05:00:12 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'sdesxwq23ws.herokuapp.com'
+      url.hostname = 'cfd43weuii.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
